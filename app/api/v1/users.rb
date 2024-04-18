043f9ef7e736e38b7f6be10986c498571ba28ddf
Naimul Islam Siam <naimul.siam@welldev.io>
2024-04-18 17:48:55 +0600

    user api

---
+++ app/api/v1/users.rb
@@ -0,0 +1,38 @@
+module API
+  module V1
+    class Users < Grape::API
+      resource :users do
+        desc 'Create a new user'
+        params do
+          requires :email, type: String
+          requires :password, type: String
+          optional :role, type: Integer
+        end
+        post do
+          user = User.new(declared(params, include_missing: false))
+
+          if user.save
+            present user, with: APIEntities::User
+          else
+            error!(user.errors.full_messages.join(', '), 400)
+          end
+        end
+
+        desc 'Sign in a user'
+        params do
+          requires :email, type: String
+          requires :password, type: String
+        end
+        post '/sign_in' do
+          user = User.find_by(email: params[:email])
+          if user&.authenticate(params[:password])
+            token = JWT.encode({ user_id: user.id }, ENV['JWT_SECRET'])
+            { token: token }
+          else
+            error!('Invalid email or password', 401)
+          end
+        end
+      end
+    end
+  end
+end
